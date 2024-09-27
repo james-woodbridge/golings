1793ea500b584ff441da02a4390e2f33b6bf4788
James Woodbridge <james.woodbridge@tpr.gov.uk>
2024-09-27 11:04:18 +0100

    completed arrays, maps, range

--- exercises/maps/maps3/main_test.go
+++ exercises/maps/maps3/main_test.go
@@ -1,7 +1,6 @@
 // maps3
 // Make me compile!
-//
-// I AM NOT DONE
+
 package main
 
 import "testing"
@@ -12,7 +11,7 @@ func TestGetPhone(t *testing.T) {
 		"John": "+01 333 666",
 	}
 
-	phone, _ := phoneBook["Anna"] // something seems wrong here
+	phone, _ := phoneBook["Ana"] // something seems wrong here
 	expectedPhone := "+01 101 102"
 	if phone != expectedPhone {
 		t.Errorf("phone should be %s but got %s", expectedPhone, phone)
@@ -21,8 +20,9 @@ func TestGetPhone(t *testing.T) {
 
 func TestInsertPhone(t *testing.T) {
 	phoneBook := map[string]string{
-		"Ana":  "+01 101 102",
-		"John": "+01 333 666",
+		"Ana":   "+01 101 102",
+		"John":  "+01 333 666",
+		"Laura": "+11 99 98 97",
 	}
 
 	phone, _ := phoneBook["Laura"] // don't change this line
@@ -38,6 +38,7 @@ func TestDeletePhone(t *testing.T) {
 		"John": "+01 333 666",
 	} // don't change the original map
 
+	delete(phoneBook, "John")
 	totalPhones := len(phoneBook)
 	expectedTotalPhones := 1
 	if totalPhones != expectedTotalPhones {
--- exercises/structs/structs1/main.go
+++ exercises/structs/structs1/main.go
@@ -7,9 +7,16 @@ package main
 import "fmt"
 
 type Person struct {
-
+	name string
+	age  int
 }
 
 func main() {
+
+	var person = Person{
+		name: "John",
+		age:  30,
+	}
+
 	fmt.Printf("Person %s and age %d", person.name, person.age)
 }
